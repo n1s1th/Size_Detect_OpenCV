9844ebb5cdc1470cf146211d6f1764e486ed5f81
Nisith Moragoda <nisith.nimdinu@gmail.com>
2025-07-07 18:58:20 +0530

    Distance Between 2 points

--- ObjectSize.py
+++ ObjectSize.py
@@ -1,9 +1,12 @@
 import cv2
+import math
 
 points = []
 def draw_circle(event, x, y, flags, params):
     global points
     if event == cv2.EVENT_LBUTTONDOWN:
+        if len(points) == 2:
+            points = []
         points.append((x, y))
 
 cv2.namedWindow("Frame")
@@ -15,6 +18,15 @@ while True:
 
     for pt in points:
         cv2.circle(frame, pt, 5, (25, 15, 255), -1)
+
+
+    #Measure the distance between two points
+    if len(points) == 2:
+        pt1 = points[0]
+        pt2 = points[1]
+        distance = math.hypot(pt2[0] - pt1[0], pt2[1] - pt1[1])
+        cv2.putText(frame, fr"{int(distance)}", (pt1[0], pt1[1] - 10), cv2.FONT_HERSHEY_PLAIN, 2.5, (25, 15, 235), 2)
+    
 
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
