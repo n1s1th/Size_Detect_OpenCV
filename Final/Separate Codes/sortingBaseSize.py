ec56bc3d589d06311753f9801ca076a3cc537815
Nisith Moragoda <nisith.nimdinu@gmail.com>
2025-08-05 00:29:42 +0530

    Sorting Base Size

---
+++ Final/Separate Codes/sortingBaseSize.py
@@ -0,0 +1,10 @@
+float x = input("Enter the first coordinate (x): ")
+float y = input("Enter the second coordinate (y): ")
+float area=x*y
+if area<20:
+    return "small"
+else:
+    return "large"
+
+
+
